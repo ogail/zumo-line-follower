3d35146b78174db8735f41d9b1f63369aee08d89
Abdelrahman Elogeel <elogeel@outlook.com>
2016-03-13 21:11:18 -0400

    Complete PID controller for line follower

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 /*
  * This example uses the ZumoMotors library to follow a black line.
+ * The 
  */
 
 #define LED_PIN                 13
@@ -19,6 +20,9 @@ unsigned int sensorValues[NUM_SENSORS];
 double Kp = 0;
 int offset = 0;
 int integral = 0;
+int derivative = 0;
+int Kd = 100; // total guess
+int lastError = 0;
 Pushbutton button(ZUMO_BUTTON);
 ZumoMotors motors;
 
@@ -54,26 +58,6 @@ void setup()
   // turn off Arduino's LED to indicate we are through with calibration
   digitalWrite(LED_PIN, LOW);
 
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  /*
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(reflectanceSensors.calibratedMinimumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(reflectanceSensors.calibratedMaximumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  Serial.println(offset);
-  */
   // Wait for the user button to be pressed and released
   button.waitForButton();
 
@@ -88,30 +72,24 @@ void setup()
   delay(1000);
 }
 
-// read calibrated sensor values and obtain a measure of the line position from 0 to 5000
 void loop()
 {
   digitalWrite(LED_PIN, HIGH);
   int position = reflectanceSensors.readLine(sensorValues);
   int error = position - offset;
+  derivative = error - lastError;
+  lastError = error;
   integral += error;
   if ((integral < 0) == (error < 0)) {
     integral = 0;
   }
-  int turn = Kp * error + integral;
+  int turn = Kp * error + integral + Kd * derivative;
   int leftSpeed = MAX_SPEED + turn;
   int rightSpeed = MAX_SPEED - turn;
-  /*
-  Serial.print(position);
-  Serial.print(' ');
-  Serial.print(leftSpeed);
-  Serial.print(' ');
-  Serial.println(rightSpeed);
-  */
 
   // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
   // Generally speaking, one motor will always be turning at MAX_SPEED
-  // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
+  // and the other will be at MAX_SPEED-|turn| if that is positive,
   // else it will be stationary.  For some applications, you might want to
   // allow the motor speed to go negative so that it can spin in reverse.
   if (leftSpeed < 0)
