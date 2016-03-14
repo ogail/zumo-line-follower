dd286071a74f1f4df9a8f7617fe8a0dc3aaa90fa
Abdelrahman Elogeel <elogeel@outlook.com>
2016-03-14 01:02:08 -0400

    Adding sounds

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
 #include <ZumoMotors.h>
+#include <ZumoBuzzer.h>
 #include <Pushbutton.h>
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
 
 /*
  * This example uses the ZumoMotors library to follow a black line.
- * The 
+ * The math behind this code is found in
+ * http://www.inpharmix.com/jps/PID_Controller_For_Lego_Mindstorms_Robots.html
  */
 
 #define LED_PIN                 13
 #define NUM_SENSORS             6  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             2  // emitter is controlled by digital pin 2
+#define MELODY_LENGTH 95
+
+// These arrays take up a total of 285 bytes of RAM (out of a limit of 1k (ATmega168), 2k (ATmega328), or 2.5k(ATmega32U4))
+unsigned char note[MELODY_LENGTH] =
+{
+  NOTE_E(5), SILENT_NOTE, NOTE_E(5), SILENT_NOTE, NOTE_E(5), SILENT_NOTE, NOTE_C(5), NOTE_E(5),
+  NOTE_G(5), SILENT_NOTE, NOTE_G(4), SILENT_NOTE,
+
+  NOTE_C(5), NOTE_G(4), SILENT_NOTE, NOTE_E(4), NOTE_A(4), NOTE_B(4), NOTE_B_FLAT(4), NOTE_A(4), NOTE_G(4),
+  NOTE_E(5), NOTE_G(5), NOTE_A(5), NOTE_F(5), NOTE_G(5), SILENT_NOTE, NOTE_E(5), NOTE_C(5), NOTE_D(5), NOTE_B(4),
+
+  NOTE_C(5), NOTE_G(4), SILENT_NOTE, NOTE_E(4), NOTE_A(4), NOTE_B(4), NOTE_B_FLAT(4), NOTE_A(4), NOTE_G(4),
+  NOTE_E(5), NOTE_G(5), NOTE_A(5), NOTE_F(5), NOTE_G(5), SILENT_NOTE, NOTE_E(5), NOTE_C(5), NOTE_D(5), NOTE_B(4),
+
+  SILENT_NOTE, NOTE_G(5), NOTE_F_SHARP(5), NOTE_F(5), NOTE_D_SHARP(5), NOTE_E(5), SILENT_NOTE,
+  NOTE_G_SHARP(4), NOTE_A(4), NOTE_C(5), SILENT_NOTE, NOTE_A(4), NOTE_C(5), NOTE_D(5),
+
+  SILENT_NOTE, NOTE_G(5), NOTE_F_SHARP(5), NOTE_F(5), NOTE_D_SHARP(5), NOTE_E(5), SILENT_NOTE,
+  NOTE_C(6), SILENT_NOTE, NOTE_C(6), SILENT_NOTE, NOTE_C(6),
+
+  SILENT_NOTE, NOTE_G(5), NOTE_F_SHARP(5), NOTE_F(5), NOTE_D_SHARP(5), NOTE_E(5), SILENT_NOTE,
+  NOTE_G_SHARP(4), NOTE_A(4), NOTE_C(5), SILENT_NOTE, NOTE_A(4), NOTE_C(5), NOTE_D(5),
+
+  SILENT_NOTE, NOTE_E_FLAT(5), SILENT_NOTE, NOTE_D(5), NOTE_C(5)
+};
+
+unsigned int duration[MELODY_LENGTH] =
+{
+  100, 25, 125, 125, 125, 125, 125, 250, 250, 250, 250, 250,
+
+  375, 125, 250, 375, 250, 250, 125, 250, 167, 167, 167, 250, 125, 125,
+  125, 250, 125, 125, 375,
+
+  375, 125, 250, 375, 250, 250, 125, 250, 167, 167, 167, 250, 125, 125,
+  125, 250, 125, 125, 375,
+
+  250, 125, 125, 125, 250, 125, 125, 125, 125, 125, 125, 125, 125, 125,
+
+  250, 125, 125, 125, 250, 125, 125, 200, 50, 100, 25, 500,
+
+  250, 125, 125, 125, 250, 125, 125, 125, 125, 125, 125, 125, 125, 125,
+
+  250, 250, 125, 375, 500
+};
+
 
 // sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
 ZumoReflectanceSensorArray reflectanceSensors;
@@ -25,6 +72,8 @@ int Kd = 100; // total guess
 int lastError = 0;
 Pushbutton button(ZUMO_BUTTON);
 ZumoMotors motors;
+ZumoBuzzer buzzer;
+unsigned char currentIdx = 0;
 
 // This is the maximum speed the motors will be allowed to turn.
 // (400 lets the motors go at top speed; decrease to impose a speed limit)
@@ -33,6 +82,9 @@ const int MAX_SPEED = 400;
 void setup()
 {
   delay(500);
+
+  // Play a little welcome song
+  buzzer.play(">g32>>c32");
 
   // Initialize the reflectance sensors module
   reflectanceSensors.init();
@@ -57,6 +109,7 @@ void setup()
   motors.setSpeeds(0,0);
   // turn off Arduino's LED to indicate we are through with calibration
   digitalWrite(LED_PIN, LOW);
+  buzzer.play(">g32>>c32");
 
   // Wait for the user button to be pressed and released
   button.waitForButton();
@@ -69,12 +122,23 @@ void setup()
   Serial.print(' ');
   Serial.println(Kp);
 
-  delay(1000);
+  // Play music and wait for it to finish before we start driving.
+  buzzer.play("L16 cdegreg4");
+  while(buzzer.isPlaying());
+  delay(500);
 }
 
 void loop()
 {
-  digitalWrite(LED_PIN, HIGH);
+  // if we haven't finished playing the song and
+  // the buzzer is ready for the next note, play the next note
+  if (currentIdx < MELODY_LENGTH && !buzzer.isPlaying())
+  {
+    // play note at max volume
+    buzzer.playNote(note[currentIdx], duration[currentIdx], 15);
+    currentIdx++;
+  }
+
   int position = reflectanceSensors.readLine(sensorValues);
   int error = position - offset;
   derivative = error - lastError;
